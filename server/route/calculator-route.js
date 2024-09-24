af211bc8df08756421a32814a8a5a883b8cb4b1e
zain100000 <muhammadzainulabideen292@gmail.com>
2024-09-24 12:33:48 +0500

    commit

--- server/route/calculator-route.js
+++ server/route/calculator-route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const calculatorController = require("../controller/calculator-controller");
+const calculatorController = require("../Controller/calculator-controller");
 
 router.post("/calculate", calculatorController.calculate);
 
